3723f356dbde5eb239bc3d8f611126aa52e9e9f8
SolidMik <86971618+SolidMik@users.noreply.github.com>
2023-06-15 13:31:09 +0100

    Counter with alert
    use states and hooks

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
 
 const App = () => {
 
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("You've changed the counter to " + counter);
+  }, [counter]);
+
   return (
     <div className="App">
-      <Person name={'John'} lastName={'Doe'} age={'25'} />
-      <Person name="Hewy" lastName="Lake" age="21"/>
-      <Person name='Em' lastName='Brown' age='23'/>
-      <Person name={'Tim'} lastName={'Left'} age={20 + 7}/>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+
     </div>
   );
 }
