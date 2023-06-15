b7a322fd16f12a70fdfebfb41733aa28a9403740
SolidMik <86971618+SolidMik@users.noreply.github.com>
2023-06-15 13:07:46 +0100

    First
    Messing around with props

---
+++ App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+const Person = (props) => {
+  return (
+    <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <Person name={'John'} lastName={'Doe'} age={'25'} />
+      <Person name="Hewy" lastName="Lake" age="21"/>
+      <Person name='Em' lastName='Brown' age='23'/>
+      <Person name={'Tim'} lastName={'Left'} age={20 + 7}/>
+    </div>
+  );
+}
+
+export default App;
