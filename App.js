94448735c1d23ac44d6e2f38327c8ce0d5101f7d
SolidMik <86971618+SolidMik@users.noreply.github.com>
2023-06-20 21:12:41 +0100

    enter button
    shows info after enter is pressed

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const UserComponent = ({ surname, age, home }) => (
+const UserComponent = ({ name, surname, age, home }) => (
   <>
-    <h1>Welcome!</h1>
+    <h1>Welcome, {name}!</h1>
     <h2>{surname}</h2>
     <h3>{age}</h3>
     <h3>{home}</h3>
@@ -13,17 +13,24 @@ const UserComponent = ({ surname, age, home }) => (
 const App = () => {
   const [inputValue, setInputValue] = useState('');
   const [userData, setUserData] = useState(null);
+  const [isEnterPressed, setIsEnterPressed] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setIsEnterPressed(true);
+    }
+  };
+
   const userMap = {
-    Mik: { surname: 'Solid', age: 26, home: 'Subotica' },
-    Jon: { surname: 'Snow', age: 16, home: 'South' },
-    Tim: { surname: 'Sloth', age: 36, home: 'CG' },
-    Beth: { surname: 'Smith', age: 23, home: 'London' },
-    Joy: { surname: 'Summer', age: 28}, home: 'Cornwall',
+    Password1: { name: 'Miki', surname: 'Solid', age: 26, home: 'Subotica' },
+    Password2: { name: 'Jon', surname: 'Snow', age: 16, home: 'South' },
+    Password3: { name: 'Tim', surname: 'Sloth', age: 36, home: 'CG' },
+    Password4: { name: 'Beth', surname: 'Smith', age: 23, home: 'London' },
+    Password5: { name: 'Joy', surname: 'Summer', age: 28, home: 'Cornwall' },
   };
 
   const setUser = (user) => {
@@ -31,8 +38,11 @@ const App = () => {
   };
 
   useEffect(() => {
-    setUser(inputValue);
-  }, [inputValue]);
+    if (isEnterPressed) {
+      setUser(inputValue);
+      setIsEnterPressed(false);
+    }
+  }, [inputValue, isEnterPressed]);
 
   return (
     <div className="App-header">
@@ -40,6 +50,7 @@ const App = () => {
         type="text"
         value={inputValue}
         onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
       />
       {userData && <UserComponent {...userData} />}
     </div>
